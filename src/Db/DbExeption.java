391baba555933082a5c02a4b5ee2114f9977013f
Paulo Henrique Congentino Ten <phcten@gmail.com>
2021-02-25 12:31:05 -0300

    Creating project and git repository

---
+++ src/Db/DbExeption.java
@@ -0,0 +1,10 @@
+package Db;
+
+public class DbExeption extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public DbExeption(String msg){
+        super(msg);
+    }
+}
